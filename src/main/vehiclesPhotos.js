f46f51bf7f85825dfd3aada703f1ce8a194dfd86
Ewa <ewzdun@gmail.com>
2019-11-26 21:22:13 +0100

    #27 Src to random img (vehicles)

--- src/main/vehiclesPhotos.js
+++ src/main/vehiclesPhotos.js
@@ -2,3 +2,24 @@ export function questionPhoto(id) {
     const src = "../../assets/img/modes/vehicles/"+id+".jpg"
     return src
 }
+
+export function rand(min, max){
+    min = parseInt( min, 10 );
+    max = parseInt( max, 10 );
+
+    if ( min > max ){
+        var tmp = min;
+        min = max;
+        max = tmp;
+    }
+    
+    return number = Math.floor( Math.random() * ( max - min + 1 ) + min )
+}
+
+export function randomPhoto() {
+    const availablePhotos = [4,6,7,8,14,16,18,19,20,24,25,26,30,33,34,35,36,37,38,42]
+    const min = 0
+    const max = 19
+    const number = rand(min, max)
+    return src = "../../assets/img/modes/vehicles/"+availablePhotos[number]+".jpg"
+}
