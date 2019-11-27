331a2f99755b1c1a2beeb789b3d46c8fd7bd8245
Ewa <ewzdun@gmail.com>
2019-11-27 20:29:35 +0100

    #26 #27 Poprawki po komentarzach

--- src/main/vehiclesPhotos.js
+++ src/main/vehiclesPhotos.js
@@ -1,25 +1,25 @@
-export function questionPhoto(id) {
-    const src = "../../assets/img/modes/vehicles/"+id+".jpg"
-    return src
+function questionPhoto(id) {
+    return "../../assets/img/modes/vehicles/" + id + ".jpg"
 }
 
-export function rand(min, max){
-    min = parseInt( min, 10 );
-    max = parseInt( max, 10 );
+function rand(min, max) {
+    min = parseInt(min, 10);
+    max = parseInt(max, 10);
 
-    if ( min > max ){
+    if (min > max) {
         var tmp = min;
         min = max;
         max = tmp;
     }
-    
-    return number = Math.floor( Math.random() * ( max - min + 1 ) + min )
+
+    return number = Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+const availablePhotos = [4, 6, 7, 8, 14, 16, 18, 19, 20, 24, 25, 26, 30, 33, 34, 35, 36, 37, 38, 42]
+
 export function randomPhoto() {
-    const availablePhotos = [4,6,7,8,14,16,18,19,20,24,25,26,30,33,34,35,36,37,38,42]
     const min = 0
     const max = 19
     const number = rand(min, max)
-    return src = "../../assets/img/modes/vehicles/"+availablePhotos[number]+".jpg"
+    return questionPhoto(availablePhotos[number])
 }
