5dbe6ec45e66dc93973122d71c3686d60ca63d85
Marek <marek_sienko@o2.pl>
2019-11-29 13:20:09 +0100

    zadanie 23 poprawki+1

--- src/main/starship.js
+++ src/main/starship.js
@@ -14,13 +14,13 @@ export default class Starship extends Mode {
         return await fetch(URL + id).then((response) => response.json()).then(res => res.name);
     }
     getRandomStarshipForModeView() {
-        const idOfStarship=this.listOfResources.splice(this.getRandomIndex(this.listOfResources.length),1);
-        const starship=this.getStarshipForId(idOfStarship);
-        return { starship , idOfStarship};
+        const id=this.listOfResources.splice(this.getRandomIndex(this.listOfResources.length),1);
+        const name=this.getStarshipForId(idOfStarship);
+        return { name , id};
     }
-    async getThreeRandomStarships(){
+    getThreeRandomStarships(){
         const clonedListOfResources=this.listOfResources.slice();
-           return await Promise.all([
+           return  Promise.all([
             fetch(URL + clonedListOfResources.splice(this.getRandomIndex(clonedListOfResources.length),1)).then((response) => response.json()).then(res => res.name),
             fetch(URL + clonedListOfResources.splice(this.getRandomIndex(clonedListOfResources.length),1)).then((response) => response.json()).then(res => res.name),
             fetch(URL + clonedListOfResources.splice(this.getRandomIndex(clonedListOfResources.length),1)).then((response) => response.json()).then(res => res.name)
