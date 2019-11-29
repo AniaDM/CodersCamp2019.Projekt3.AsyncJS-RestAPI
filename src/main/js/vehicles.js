bf0585443bf12ddc54af55b423574b99220323ec
Piotr <bozicss@gazeta.pl>
2019-11-30 00:30:53 +0100

    Vehicles - selection and uploading on page 3 answers

---
+++ src/main/js/vehicles.js
@@ -0,0 +1,85 @@
+const URL = 'https://swapi.co/api/vehicles/';
+const LIST = [4, 6, 7, 8, 14, 16, 18, 19, 20, 24, 25, 26, 30, 33, 34, 35, 36, 37, 38, 42];
+const ASSETSV = '../../assets/img/modes/vehicles/'
+
+const answerV1 = document.querySelector('.answer1');
+const answerV2 = document.querySelector('.answer2');
+const answerV3 = document.querySelector('.answer3');
+const answerV4 = document.querySelector('.answer4');
+pictureQuiz = document.querySelector('.lord-vader-pic');
+const vehicleNumbers = [0, 0, 0];
+const vehicle = [];
+
+function getRandomIndex(length) {
+    return Math.floor(Math.random() * length)
+}
+
+const vehicleSelection = LIST.splice(getRandomIndex((LIST.length)), 1);
+console.log(vehicleSelection);
+
+pictureQuiz.src = `${ASSETSV}${vehicleSelection}.jpg`;
+
+for (let i = 0; i < 3; i++) {
+    vehicleNumbers[i] = LIST[getRandomIndex(LIST.length)];
+    if (vehicleNumbers[0] === vehicleNumbers[1] || vehicleNumbers[0] === vehicleNumbers[2] || vehicleNumbers[0] === vehicleNumbers[2]) {
+        i--
+    }
+}
+
+const vehicle0 = fetch(`${URL}${vehicleSelection}`)
+    .then(response => response.json())
+    .then(data => data.name)
+    .catch(err => console.log(error))
+
+vehicle1 = fetch(`${URL}${vehicleNumbers[0]}`)
+    .then(response => response.json())
+    .then(data => data.name)
+    .catch(err => console.log(error)),
+
+    vehicle2 = fetch(`${URL}${vehicleNumbers[1]}`)
+    .then(response => response.json())
+    .then(data => data.name)
+    .catch(err => console.log(error)),
+
+    vehicle3 = fetch(`${URL}${vehicleNumbers[2]}`)
+    .then(response => response.json())
+    .then(data => data.name)
+    .catch(err => console.log(error)),
+
+    Promise.all([vehicle0, vehicle1, vehicle2, vehicle3])
+    .then(result => {
+        vehicle.push(result);
+        const vehicleChange = vehicle[0];
+
+        let randomNumber = getRandomIndex(4); //test - zmienić nazwę zmiennej
+
+        console.log(randomNumber); //sprawdzenie
+
+        answerV1.innerText = vehicleChange[randomNumber];
+        if (randomNumber === 3) {
+            randomNumber = 0
+        } else randomNumber++
+
+        answerV2.innerText = vehicleChange[randomNumber];
+        if (randomNumber === 3) {
+            randomNumber = 0
+        } else randomNumber++
+
+        answerV3.innerText = vehicleChange[randomNumber];
+        if (randomNumber === 3) {
+            randomNumber = 0
+        } else randomNumber++
+
+        answerV4.innerText = vehicleChange[randomNumber];
+        if (randomNumber === 3) {
+            randomNumber = 0
+        } else randomNumber++
+
+        console.log(vehicleChange[0]); //sprawdzenie
+
+        answerV1.style.color = 'black';
+        answerV2.style.color = 'black';
+        answerV3.style.color = 'black';
+        answerV4.style.color = 'black';
+
+    });
