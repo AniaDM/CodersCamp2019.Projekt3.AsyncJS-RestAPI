1f00daf2a522e54120c87b5499279ffc4100917b
mnowak <mateusz.nowak@intenso.pl>
2019-12-01 13:06:13 +0100

    #12 Usage sample of google with random person.

--- src/main/samples/randomPersonTest.js
+++ src/main/samples/randomPersonTest.js
@@ -5,6 +5,10 @@ export async function randomPersonTest() {
     const person = await randomPerson();
     const recognizedPerson = await recognizePhoto("people", person.id);
     const isGoogleCorrect = person.name === recognizedPerson;
-    console.table([person.name, recognizedPerson, isGoogleCorrect]);
+    console.table({
+        'Person Name': person.name,
+        'Google Vision API Answer': recognizedPerson,
+        'Is Vision API correct': isGoogleCorrect
+    });
     return isGoogleCorrect;
 }
